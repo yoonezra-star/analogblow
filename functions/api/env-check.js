9dbbababeba289f3fd1a9c5ade3ebbf465b4e4fc
yoonezra-star <yoonezra@gmail.com>
2026-05-22 22:17:37 +0900

    Add temporary environment diagnostics

---
+++ functions/api/env-check.js
@@ -0,0 +1,14 @@
+export async function onRequestGet({ env }) {
+  const keys = Object.keys(env || {}).sort();
+  return new Response(JSON.stringify({
+    keys,
+    hasNaverId: keys.includes('NAVER_SEARCH_CLIENT_ID'),
+    hasNaverSecret: keys.includes('NAVER_SEARCH_CLIENT_SECRET'),
+    hasDataGoKr: keys.includes('DATA_GO_KR_SERVICE_KEY')
+  }), {
+    headers: {
+      'content-type': 'application/json; charset=utf-8',
+      'cache-control': 'no-store'
+    }
+  });
+}
